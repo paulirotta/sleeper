0537d30ac91ea4a880cfc965e9ff7cfa252f92c8
Paul Houghton <paulirotta@gmail.com>
2018-11-17 13:13:49 +0200

    Sweep on 2 channels

--- sleeper/src/main.rs
+++ sleeper/src/main.rs
@@ -1,5 +1,41 @@
-// extern crate rppal;
+extern crate rppal;
+
+use rppal::pwm::*;
+use core::time::Duration;
+use std::thread::sleep;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, sleeper!");
+
+    let period = Duration::from_micros(10000);
+    let duty_cycle = Duration::from_micros(10000);
+
+    let throttle = Pwm::with_period(
+    Channel::Pwm0, 
+    period, 
+    duty_cycle, 
+    Polarity::Normal, 
+    true).expect("Could not initialize throttle");
+
+    let steering = Pwm::with_period(
+    Channel::Pwm1, 
+    period, 
+    duty_cycle, 
+    Polarity::Normal, 
+    true).expect("Could not initialize steering");
+
+    let delay = 2000;
+    let delay2 = 1;
+    let mut val = 1000;
+    sleep(Duration::from_millis(delay));
+    
+    for val2 in 0..3000 {
+        let d = val2;
+        throttle.set_duty_cycle(Duration::from_micros(d));
+        steering.set_duty_cycle(Duration::from_micros(d));        
+        println!("{} micros", d);
+        sleep(Duration::from_millis(delay2));
+    }
+
+    println!("Bye");
 }
