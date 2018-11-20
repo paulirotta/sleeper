c293222bcabbd0d919ceb0718c323c75133e109a
Paul Houghton <paulirotta@gmail.com>
2018-11-20 16:48:14 +0200

    Updating module structure WIP

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # sleeper
 Autonomous drift car experiment
 
+1. Visit http://rustup.rs 
+
+2. On Raspberry Pi: 
+
+Instructions at https://docs.golemparts.com/rppal/0.9.0/rppal/spi/index.html which are, briefly:
+A. Add dtoverlay=pwm-2chan to /boot/config.txt
+B. Append the following snippet to /etc/udev/rules.d/99-com.rules. Reboot the Raspberry Pi afterwards.
+
+SUBSYSTEM=="pwm*", PROGRAM="/bin/sh -c '\
+    chown -R root:gpio /sys/class/pwm && chmod -R 770 /sys/class/pwm;\
+    chown -R root:gpio /sys/devices/platform/soc/*.pwm/pwm/pwmchip* &&\
+    chmod -R 770 /sys/devices/platform/soc/*.pwm/pwm/pwmchip*\
+'"
+
+3. sudo apt install libatlas-base-dev
+
+4. pip3 install tensorflow
+
+WIP 5. You can run on a PC without PWM hardware since the "gpio" feature is off by default. On a Pi, use
+    cargo run --feature "gpio" --release
+
--- sleeper/src/main.rs
+++ sleeper/src/main.rs
@@ -1,40 +1,36 @@
+#[cfg(feature = "gpio")]
 extern crate rppal;
 
-use rppal::pwm::*;
 use core::time::Duration;
+#[cfg(feature = "gpio")]
+use rppal::pwm::*;
 use std::thread::sleep;
 
 fn main() {
     println!("Hello, sleeper!");
 
-    let period = Duration::from_micros(10000);
-    let duty_cycle = Duration::from_micros(10000);
+    if cfg!(features = "gpio") {
+        let period = Duration::from_micros(10000);
+        let duty_cycle = Duration::from_micros(10000);
 
-    let throttle = Pwm::with_period(
-    Channel::Pwm0, 
-    period, 
-    duty_cycle, 
-    Polarity::Normal, 
-    true).expect("Could not initialize throttle");
+        let throttle = Pwm::with_period(Channel::Pwm0, period, duty_cycle, Polarity::Normal, true)
+            .expect("Could not initialize throttle");
 
-    let steering = Pwm::with_period(
-    Channel::Pwm1, 
-    period, 
-    duty_cycle, 
-    Polarity::Normal, 
-    true).expect("Could not initialize steering");
+        let steering = Pwm::with_period(Channel::Pwm1, period, duty_cycle, Polarity::Normal, true)
+            .expect("Could not initialize steering");
 
-    let delay = 2000;
-    let delay2 = 1;
-    let mut val = 1000;
-    sleep(Duration::from_millis(delay));
-    
-    for val2 in 0..3000 {
-        let d = val2;
-        throttle.set_duty_cycle(Duration::from_micros(d));
-        steering.set_duty_cycle(Duration::from_micros(d));        
-        println!("{} micros", d);
-        sleep(Duration::from_millis(delay2));
+        let delay = 2000;
+        let delay2 = 1;
+        let mut val = 1000;
+        sleep(Duration::from_millis(delay));
+
+        for val2 in 0..3000 {
+            let d = val2;
+            throttle.set_duty_cycle(Duration::from_micros(d));
+            steering.set_duty_cycle(Duration::from_micros(d));
+            println!("{} micros", d);
+            sleep(Duration::from_millis(delay2));
+        }
     }
 
     println!("Bye");
