a66dda505c3c6de3b0def462ec57c433b4f5901a
Paul Houghton <paulirotta@gmail.com>
2018-11-22 08:44:30 +0200

    gpio.rs minor

--- sleeper/src/gpio.rs
+++ sleeper/src/gpio.rs
@@ -17,8 +17,7 @@ pub fn print_hello() {
     let period = Duration::from_micros(10000);
     let duty_cycle = Duration::from_micros(10000);
 
-    let throttle =
-        rppal::pwm::Pwm::with_period(Channel::Pwm0, period, duty_cycle, Polarity::Normal, true)
+    let throttle = rppal::pwm::Pwm::with_period(Channel::Pwm0, period, duty_cycle, Polarity::Normal, true)
             .expect("Could not initialize throttle");
 
     let steering = Pwm::with_period(Channel::Pwm1, period, duty_cycle, Polarity::Normal, true)
